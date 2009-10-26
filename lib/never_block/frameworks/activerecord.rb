eeed6ef95579b0cbe97d74e48eacc07b765aa60d
desty-nova <desty-nova@hotmail.fr>
2009-10-26 10:36:05 +0100

    ...

--- lib/never_block/frameworks/activerecord.rb
+++ lib/never_block/frameworks/activerecord.rb
@@ -2,6 +2,7 @@ require 'activerecord'
 
 # We also need to override the scoped methods to store
 # the scope in the fiber context
+
 class ActiveRecord::Base
   def scoped_methods #:nodoc:
     Fiber.current[:"#{self}_scoped_methods"] ||= self.default_scoping.dup
